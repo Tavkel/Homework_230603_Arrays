a5444f00a0447c031deeed86233016a58f5ee2d4
Tavkel <gmininb@gmail.com>
2023-06-04 19:44:55 +0300

    Выполненное домашее задание от 23.06.03

---
+++ src/Data.java
@@ -0,0 +1,25 @@
+public class Data
+{
+	private Integer[] intArray;
+	private Double[] doubleArray;
+	private Character[] charArray;
+
+	public Data(Integer[] intArray, Double[] doubleArray, Character[] charArray)
+	{
+		this.intArray = intArray;
+		this.doubleArray = doubleArray;
+		this.charArray = charArray;
+	}
+	public Integer[] getIntArray()
+	{
+		return intArray;
+	}
+	public Double[] getDoubleArray()
+	{
+		return doubleArray;
+	}
+	public Character[] getCharArray()
+	{
+		return charArray;
+	}
+}
